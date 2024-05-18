309f271f44d257bce72c4d6c10c28a57c0dcc38b
cheese <hariwong-cloud@hotmail.com>
2024-05-18 11:57:24 +0700

    new item + block

--- src/main/java/net/cheese/mymod/block/ModBlocks.java
+++ src/main/java/net/cheese/mymod/block/ModBlocks.java
@@ -2,10 +2,12 @@ package net.cheese.mymod.block;
 
 import net.cheese.mymod.Mymod;
 import net.cheese.mymod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,10 +20,36 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Mymod.MOD_ID);
 
+    //Block
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
     public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+    //ORE
+    public static final RegistryObject<Block> SAPPHIRE_ORE = registerBlock("sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE)
+                    .strength(2.0F, 2.0F).requiresCorrectToolForDrops(),
+                    UniformInt.of(3,6)));
+
+    public static final RegistryObject<Block> DEEPSLATE_SAPPHIRE_ORE = registerBlock("deepslate_sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE)
+                    .strength(3.0F, 3.0F).requiresCorrectToolForDrops(),
+                    UniformInt.of(3,6)));
+
+    public static final RegistryObject<Block> NETHER_SAPPHIRE_ORE = registerBlock("nether_sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE)
+                    .strength(1.0F, 1.0F).requiresCorrectToolForDrops(),
+                    UniformInt.of(3,6)));
+
+    public static final RegistryObject<Block> END_STONE_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE)
+                    .strength(3.0F, 3.0F).requiresCorrectToolForDrops(),
+                    UniformInt.of(3,6)));
+
+
+
 
 
 
--- src/main/java/net/cheese/mymod/item/ModCreativeModTabs.java
+++ src/main/java/net/cheese/mymod/item/ModCreativeModTabs.java
@@ -21,6 +21,10 @@ public class ModCreativeModTabs {
                         output.accept(ModItems.RAW_SAPPHIRE.get());
                         output.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         output.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+                        output.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
                     }).build());
 
     public static void register(IEventBus eventBus) {
